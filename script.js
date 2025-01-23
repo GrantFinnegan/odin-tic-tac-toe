113b4220792f24300e87fc2fd6e0fabf0c75425b
Grant Finnegan <grantfinnegan@gmail.com>
2025-01-23 12:40:05 -0600

    Add comments to GameController

--- script.js
+++ script.js
@@ -14,12 +14,17 @@ function GameController (xPlayerName = "X-player", oPlayername = "O-player") {
         return {boardState, activePlayer}
     }
 
+    //initialize GameController Data
     let xPlayer = Player(xPlayerName);
     let oPlayer = Player(oPlayername);
     let gameBoard = GameBoard(xPlayer);
 
 
     function printBoardState() {
+        //Example printout:
+        // |x| | |
+        // | |o| |
+        // |x| |o|
         let printOut =
             "|" + gameBoard.boardState[0] + "|" + gameBoard.boardState[1] + "|" + gameBoard.boardState[2] + "|\n" +
             "|" + gameBoard.boardState[3] + "|" + gameBoard.boardState[4] + "|" + gameBoard.boardState[5] + "|\n" +
